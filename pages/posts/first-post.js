0ef916a79dc96af089d7062c0c0ec5fa646d05b5
Sajadmhy <sajad.mahyaei@gmail.com>
2022-05-16 07:08:27 -0400

    Add styles and profile layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,10 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
 import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -17,17 +18,11 @@ export default function FirstPost() {
                 }
             />
             <h1>First Post</h1>
-            <Image
-            src='/images/profile.jpg'
-            height={144}
-            width={144}
-            alt='Profile Pic'
-            />
             <h2>
                 <Link href='/'>
                     <a>Back to home</a>
                 </Link>
             </h2>
-        </>
+        </Layout>
     ); 
 }
